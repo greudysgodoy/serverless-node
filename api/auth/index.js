36d96c515ffa8814c9e3a9f47a8bb3ba19c877ad
greudysgodoy <greudysgodoy@gmail.com>
2021-08-24 12:58:59 -0400

    init

---
+++ api/auth/index.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const Users = require('../models/Users')
+
+const isAuthenticated = (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) {
+    return res.sendStatus(403)
+  }
+  jwt.verify(token, 'my-secret', (err, decoded) => {
+    const { _id } = decoded
+    Users.findOne({ _id }).exec()
+      .then(user => {
+        req.user = user
+        next()
+      })
+  })
+}
+
+const hasRole = role => (req, res, next) => {
+  if (req.user.role === role) {
+    return next()
+  }
+  res.sendStatus(403)
+}
+
+module.exports = {
+  isAuthenticated,
+  hasRole,
+}
